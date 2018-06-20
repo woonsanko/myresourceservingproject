549378ec17c7a14041487df77ead0785dd31fcd5
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-06-20 17:37:33 -0400

    Step 0: creating skeletal hippo 12.1 project adding news and events only through essentials

---
+++ site/src/main/java/org/example/hst/resource/serving/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.example.hst.resource.serving.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="myresourceservingproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
